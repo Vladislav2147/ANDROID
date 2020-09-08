85b2fa610a93555f3a220b450c796fd4a902fcdf
Vladislav2147 <vandl3511@gmail.com>
2020-09-08 14:46:39 +0300

    decided to go to 3lr

--- lr2/src/by/bstu/shichko/fit/manager/Action.java
+++ lr2/src/by/bstu/shichko/fit/manager/Action.java
@@ -1,18 +1,37 @@
 package by.bstu.shichko.fit.manager;
 
 import by.bstu.shichko.fit.course.Course;
+import by.bstu.shichko.fit.person.Person;
+import by.bstu.shichko.fit.person.Student;
+
+import java.io.File;
+import java.util.List;
 
 public interface Action {
-
-    Course course = null;
 
     static String info() {
         return "Interface Action contains methods for working with course";
     }
 
-    default Course generateCourse() {
+    Course generateCourse(String courseName, String organisationName, int studentsAmount);
+    Course generateCourse(String courseName, String organisationName, String fileName);
+
+    default Person personGenerator() {
+        return new Student();
+    }
+    
+    class FileManager {
+
+        public File getFileFromName(String filename) {
+            return new File(filename);
+        }
+
+        public List<String> getFirstNames() {
+            return List.of("Ivan", "Michael", "Vladimir", "Anton", "Nikolai", "Nikita", "Paul");
+        }
+        public List<String> getSecondNames() {
+            return List.of("Ivanov", "Michalov", "Vorobiev", "Lisunov", "Fursik", "Nikitin", "Golubev");
+        }
 
     }
-
-    
 }
