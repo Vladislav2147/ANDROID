69f4106791137044eb84706aada927b2aaa56144
Vladislav2147 <vandl3511@gmail.com>
2020-09-14 19:46:43 +0300

    lr3: optimized imports

--- lr3/app/src/main/java/by/bstu/svs/fit/lr3/manager/Manager.java
+++ lr3/app/src/main/java/by/bstu/svs/fit/lr3/manager/Manager.java
@@ -1,16 +1,9 @@
 package by.bstu.svs.fit.lr3.manager;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -20,6 +13,8 @@ import by.bstu.svs.fit.lr3.person.Person;
 
 public class Manager implements Action {
 
+    //TODO lambdas
+    //TODO sorting method and comparator
 
     @Override
     public Course generateCourse(File file) throws IOException {
